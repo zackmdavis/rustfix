c6f276c4b400802c8b45d2a4308337a13d15a007
Zack M. Davis <zack@thecoalition.com>
2018-08-18 17:56:51 -0700

    WIP

--- tests/edge_cases.rs
+++ tests/edge_cases.rs
@@ -2,11 +2,20 @@ extern crate rustfix;
 use std::collections::HashSet;
 use std::fs;
 
+use rustfix::apply_suggestions;
+
+
 #[test]
-fn multiple_fix_options_yield_no_suggestions() {
-    let json = fs::read_to_string("./tests/edge-cases/skip-multi-option-lints.json").unwrap();
-    let expected_suggestions =
+fn overlapping_suggestions_not_fixable() {
+    let json = fs::read_to_string("./tests/edge-cases/overlapping-suggestions-not-fixable.json").unwrap();
+    let suggestions =
         rustfix::get_suggestions_from_json(&json, &HashSet::new(), rustfix::Filter::Everything)
-            .unwrap();
-    assert!(expected_suggestions.is_empty());
+        .unwrap();
+    let code = fs::read_to_string("./tests/edge-cases/overlapping-suggestions-not-fixable.rs").unwrap();
+    let fix_result = apply_suggestions(&code, &suggestions);
+    let fix_err = fix_result.unwrap_err();
+    assert_eq!(
+        "Cannot replace slice of data that was already replaced",
+        fix_err.to_string()
+    );
 }
